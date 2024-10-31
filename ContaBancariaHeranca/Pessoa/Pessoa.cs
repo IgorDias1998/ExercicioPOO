27b706fb930d5a633819fccd4197cb7dc289cf19
IgorDias1998 <igordiass1998@gmail.com>
2024-10-30 23:07:04 -0300

    Exercício Classe Pessoa com Sobreccarga de construtor

---
+++ ContaBancariaHeranca/Pessoa/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿namespace Pessoa
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa()
+        {
+        }
+        
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Nome: {Nome} - Idade: {Idade}");
+        }
+    }
+}
---
+++ ContaBancariaHeranca/Pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Pessoa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+
+            pessoa1.Nome = "Igor";
+            pessoa1.Idade = 26;
+        }
+    }
+}
